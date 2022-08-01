b2d7bcb086e0f7d4d109192e4559bccffa68eb76
Developer-fstudio <developer@fstudio.pt>
2022-08-01 14:12:15 +0100

    update Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Models\Empresa;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,8 +25,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+
+        $empresa = Empresa::find(1);
         // $schedule->command('inspire')->hourly();
-        $schedule->command('Aniversarios:msg')->everyMinute();
+    $schedule->command('Aniversarios:msg')->dailyAt('09:00'/*$empresa->horaMsgAniversario*/);
     }
 
     /**
